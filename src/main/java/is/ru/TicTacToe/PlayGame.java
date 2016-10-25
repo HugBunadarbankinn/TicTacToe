86b399cec0d32e98ccaa13b80735f8a220a8e0ca
Hrafnhildur Skúladóttir <hrafnhildursku15@ru.is>
2016-10-25 18:45:34 +0000

    added more test for isValidNumber and isSlotFree

--- src/main/java/is/ru/TicTacToe/PlayGame.java
+++ src/main/java/is/ru/TicTacToe/PlayGame.java
@@ -1,20 +1,18 @@
 package is.ru.TicTacToe;
 
 public class PlayGame {
-    private GameBoard board = new GameBoard();
+    protected GameBoard board = new GameBoard();
 
     protected boolean isValidNumber(int num){
-        if(num <= 0 || num > 9 )
-        {
+        if(num <= 0 || num > 9 ){
             return false;
         }
         return true;
     }
     protected boolean isSlotFree(int col, int row){
-        if (board.board[col][row] != '-') {
+        if(board.board[col][row] == 'X' || board.board[col][row] == 'O') {
             return false;
         }
         return true;
-
     }
 }
